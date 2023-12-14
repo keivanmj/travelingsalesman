d4e5837d0accac4c05ae47238533c75eb2642ae2
Ali Mirzaei <93037774+AliMirzaei02@users.noreply.github.com>
2023-12-15 00:16:41 +0330

    add find_successors function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import queue
 
 
 def add_free_items(old_item):
@@ -16,24 +16,43 @@ def add_free_items(old_item):
     Ice_cream = 12
     
     if "C" == old_item[-1]:
-        return int(old_item[:-1]) + Coffee
+        return str(int(old_item[:-1]) - Coffee)
     elif "B" == old_item[-1]:
-        return int(old_item[:-1]) + Biscuit
+        return str(int(old_item[:-1]) - Biscuit)
     elif "I" == old_item[-1]:
-        return int(old_item[:-1]) + Ice_cream
+        return str(int(old_item[:-1]) - Ice_cream)
     elif "R" == old_item[-1]:
-        return int(old_item[:-1])
+        return str(old_item[:-1])
     elif "T" == old_item[-1]:
-        return int(old_item[:-1])
+        return str(old_item[:-1])
     else:
-        return old_item    
+        return old_item
 
 
 
-Rows = int(input("Give the number of rows:"))
-Columns = int(input("Give the number of columns:"))
-matrix = np.array([list(map(str, input().split())) for _ in range(Rows)])
+def find_successors(matrix, position):
+    positions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+    moves = ["L", "R", "U", "D"]
+    successors = []
+    for direction, pos in enumerate(positions):
+        new_x, new_y = position[0] + pos[0], position[1] + pos[1]
+        if 0 <= new_x < matrix.shape[0] and 0 <= new_y < matrix.shape[1] and matrix[new_x, new_y] != "X":
+            successors.append(moves[direction])
+    return successors
+
+
+
+
+#Rows = int(input("Give the number of rows:"))
+#Columns = int(input("Give the number of columns:"))
+#matrix = np.array([list(map(str, input().split())) for _ in range(Rows)])
+#print(matrix)
+
+#cost_matrix = list(map(lambda row: list(map(add_free_items, row)), matrix))
+#print(cost_matrix)
+
+matrix = np.array([["2R", "X", "5T"], ["4C", "3", "7I"]])
 print(matrix)
-
-cost_matrix = matrix = list(map(lambda row: list(map(add_free_items, row)), matrix))
+cost_matrix = list(map(lambda row: list(map(add_free_items, row)), matrix))
 print(cost_matrix)
+print(find_successors(matrix, (1, 0)))
