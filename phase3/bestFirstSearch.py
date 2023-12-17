80f35011f77d2e28763857683657c7131f59e09e
keivanmj <k1majnoughty@gmail.com>
2023-12-17 18:32:41 +0330

    fixed best first search

--- phase3/bestFirstSearch.py
+++ phase3/bestFirstSearch.py
@@ -4,19 +4,17 @@ import time
 import functions.functions as f
 
 def bestFirstSearch(matrix) :
-
     """This is a bestFirstSearch search algorithm that returns the shortest path from start point to any other points of the matrix
     """
     start = time.time()
     pq = PriorityQueue()
     pq_end = PriorityQueue()
     path = ""
-    pq_end.put((f.heuristic(matrix, path), path))
-    pq.put((f.heuristic(matrix, path), path))
+    pq_end.put((f.calculate_heuristic(matrix, path), path))
+    pq.put((f.calculate_heuristic(matrix, path), path))
     visited = set()
     visited_items = ""
     f.print_matrix(matrix)
-    c = 0
     while pq_end.get() != 0 :
         cost, path = pq.get()
         if f.is_job_done(matrix, path):
@@ -30,36 +28,6 @@ def bestFirstSearch(matrix) :
         for move in ["L", "R", "U", "D"]:
             newpath = path + move
             if move in f.find_successors(matrix, f.find_location(matrix, path)):
-                pq.put(((f.heuristic(matrix, newpath)), newpath))
-                print(pq.queue)
+                pq.put(((f.calculate_heuristic(matrix, newpath)), newpath))
         pq_end.put(pq.queue[0])
-        print(f"##{pq_end.queue}")
-
-    end = time.time()
     return "No routes found!"
-
-choise = str(input("Do you want to enter the matrix manually(True) or use the samples(False)?"))
-if choise == "True":
-    Rows = int(input("Give the number of rows:"))
-    Columns = int(input("Give the number of columns:"))
-    matrix = np.array([list(map(str, input().split())) for _ in range(Rows)])
-elif choise == "False":
-    sample_number = int(input("(3, 3) -> 0\n(3, 5) -> 1\n(6, 5) -> 2\nchoose one of those samples:"))
-    if sample_number == 0:
-        matrix = np.array([["5", "2T", "1"], ["2R", "5", "X"], ["4C", "3T", "7I"]])
-    elif sample_number == 1:
-        matrix = np.array([["5", "3C", "9I", "25", "1"], ["2R", "X", "3T", "X", "5T"], ["4C", "4", "2", "3", "7I"]])
-    elif sample_number == 2:
-        matrix = np.array([["4", "2C", "1", "15", "1B"], ["5", "4", "5", "X", "X"]
-                         , ["2", "2", "1", "1R", "1T"], ["5", "2", "1", "1", "X"]
-                         , ["50", "2", "1C", "1", "X"], ["2T", "2", "1", "1", "1"]])
-#
-print(bestFirstSearch(matrix))
-
-# matrix = np.array([["4", "2C", "1", "15", "1B"], ["5", "4", "5", "X", "X"]
-#                  , ["2", "2", "1", "1R", "1T"], ["5", "2", "1", "1", "X"]
-#                  , ["50", "2", "1C", "1", "X"], ["2T", "2", "1", "1", "1"]])
-# # matrix = np.array([["5", "2T", "1"], ["2R", "5", "X"], ["4C", "3T", "7I"]])
-#
-# f.print_matrix(matrix)
-# print(f.heuristic(matrix, "RL"))
