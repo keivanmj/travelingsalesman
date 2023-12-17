1b2cffc10422ced4e0d9dde787f39075ddaeb635
Ali Mirzaei <93037774+AliMirzaei02@users.noreply.github.com>
2023-12-17 13:07:54 +0330

    fix print_matrix

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ def print_matrix(matrix, moves=""):
         elif move == "D":
             i += 1
         pos.add((i, j))    #  '─', '│', '┌', '│', '└', '│', '├', '─', '─', '┐', '┬', '┘', '┴', '┤', '┼'
-    print("┌" + "─────┬"*len(matrix[0]))
+    print("┼" + "─────┼"*len(matrix[0]))
     for i, row in enumerate(matrix):
         print("│", end="")
         for j, val in enumerate(row):
@@ -183,7 +183,7 @@ def print_matrix(matrix, moves=""):
                 print("{:^5}".format("+"), end = "│")
             else:
                 print("{:^5}".format(val), end = "│")
-        print("\n├" + "─────┼"*len(matrix[0]))
+        print("\n┼" + "─────┼"*len(matrix[0]))
 
 
 
@@ -215,7 +215,7 @@ def breadthFirstSearch(matrix):
 
 def depthFirstSearch(matrix):
     """This is a Depth-first search algorithm that returns the shortest path from start point to any other points of the matrix
-        """
+    """
     start = time.time()
     s = LifoQueue()
     path = ""
@@ -240,8 +240,8 @@ def depthFirstSearch(matrix):
 
 
 def IterativeDeepeningSearch(matrix):
-    """This is a Depth-first search algorithm that returns the shortest path from start point to any other points of the matrix
-        """
+    """This is a Iterative-deepening search algorithm that returns the shortest path from start point to any other points of the matrix
+    """
     start = time.time()
     s = LifoQueue()
     path = ""
@@ -249,7 +249,7 @@ def IterativeDeepeningSearch(matrix):
     c = 0
     while not (is_job_done(matrix, path)):
         path = s.get()
-        if (len(path) == 0) :
+        if (len(path) == 0):
             c = c + 1
             visited = set()
             visited_items = ""
@@ -259,14 +259,14 @@ def IterativeDeepeningSearch(matrix):
             continue
         visited_items += item_check(matrix, path)
         visited.add((i, j, visited_items))
-        if (len(path) < c) :
+        if (len(path) < c):
             for move in ["L", "R", "U", "D"]:
                 newpath = path + move
-                if move in p1.find_successors(matrix, find_location(matrix, path)):
+                if move in find_successors(matrix, find_location(matrix, path)):
                     s.put(newpath)
     end = time.time()
     print_matrix(matrix)
-    return ( (500 - calculate_cost(matrix, path)), path, (end - start))
+    return ((500 - calculate_cost(matrix, path)), path, (end - start))
 
 
 
@@ -317,4 +317,5 @@ elif choise == "False":
 
 print(breadthFirstSearch(matrix))
 print(depthFirstSearch(matrix))
+print(IterativeDeepeningSearch(matrix))
 print(uniformCostSearch(matrix))
